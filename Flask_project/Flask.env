e3fad096e7662981f0a62dea8f8146f7fb81b00d
XY <2569469269@qq.com>
2021-08-20 09:43:07 +0800

    Flask_project/

--- Flask_project/Flask.env
+++ Flask_project/Flask.env
@@ -1,2 +1,1 @@
 FLASK_APP=microblog.py
-
--- Flask_project/app/__init__.py
+++ Flask_project/app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config#从config模块导入Config类
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-from app import routes
+login = LoginManager(app)#初始化Flask-Login
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+login = LoginManager(app)
+login.login_view = 'login'
+from app import routes,models
